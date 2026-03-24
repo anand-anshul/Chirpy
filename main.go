81ba2ce0e41f66c1fca62d42a2cc8cf1c14a7d9b
anandanshul <anshulanand194@gmail.com>
2026-03-24 11:15:43 +0000

    implemented handling json req res

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
@@ -16,9 +22,10 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
-	mux.HandleFunc("GET /healthz", handlerRediness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerRediness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -36,10 +43,6 @@ func handlerRediness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
@@ -48,14 +51,84 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	reqCount := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
-	w.Write([]byte(reqCount))
+	htmlString := `
+		<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>
+	`
+	reqCountHtml := fmt.Sprintf(htmlString, cfg.fileserverHits.Load())
+	w.Write([]byte(reqCountHtml))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits reset to 0"))
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(code)
+	w.Write(response)
+
+	return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+	type responseBody struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	params := requestBody{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	cleanedBody := cleanString(params.Body)
+
+	response := responseBody{
+		CleanedBody: cleanedBody,
+	}
+	respondWithJSON(w, 200, response)
+}
+
+func cleanString(s string) string {
+	sSlice := strings.Split(s, " ")
+	for i, word := range sSlice {
+		if strings.ToLower(word) == "kerfuffle" {
+			sSlice[i] = "****"
+		} else if strings.ToLower(word) == "fornax" {
+			sSlice[i] = "****"
+		} else if strings.ToLower(word) == "sharbert" {
+			sSlice[i] = "****"
+		}
+	}
+	cleanString := strings.Join(sSlice, " ")
+	return cleanString
 }
